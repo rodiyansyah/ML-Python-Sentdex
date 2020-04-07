6c89a8164535c65d462dc82348255f865a057990
Sandi Fajar Rodiyansyah <galuh29@gmail.com>
2020-04-07 08:34:00 +0700

    Mac Github

--- tut1.py
+++ tut1.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import quandl
-
 quandl.ApiConfig.api_key = "WLJMuhZbyKPneqQNBEAZ"
-mydata = quandl.get("WIKI/GOOGL")
+df = quandl.get("WIKI/GOOGL")
 
-print(mydata.head())
+df = df[['Adj. Open','Adj. High','Adj. Low','Adj. Close','Adj. Volume', ]]
+
+df['HL_PCT'] =(df['Adj. High']-df['Adj. Close']) / df['Adj. Close'] * 100.0
+df['PCT_ch'] =(df['Adj. Close']-df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close','HL_PCT','PCT_ch', 'Adj. Volume']]
+
+print(df)
