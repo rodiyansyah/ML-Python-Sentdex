e89dbbdde2ac7b7edd99fa98e888eeabeef7f127
Sandi Fajar Rodiyansyah <galuh29@gmail.com>
2020-04-07 11:07:04 +0700

    Mac

--- tut1.py
+++ tut1.py
@@ -3,8 +3,10 @@ import quandl, math
 import numpy as np
 from sklearn import preprocessing 
 from sklearn.model_selection import cross_validate
+from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.linear_model import LinearRegression
+
 
 
 
@@ -23,18 +25,20 @@ forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
 forecast_out = int(math.ceil(0.01*len(df)))
+print(forecast_out)
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
 
 x = np.array(df.drop(['label'],1))
 y =np.array(df['label'])
-
 x = preprocessing.scale(x)
-
-#x = x[:-forecast_out+1]
-df.dropna(inplace=True)
 y = np.array(df['label'])
 
-print(len(x), len(y))
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2)
+
+clf = LinearRegression(n_jobs=-1)
+clf.fit(x_train, y_train)
+conf = clf.score(x_test, y_test)
+print(conf)
 
 
