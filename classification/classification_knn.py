18015fe2150ac40e30486a9e227d245de88df1e2
Sandi Fajar Rodiyansyah <galuh29@gmail.com>
2020-04-29 16:22:58 +0700

    mac

---
+++ classification/classification_knn.py
@@ -0,0 +1,26 @@
+import numpy as np
+from sklearn import preprocessing, neighbors
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import train_test_split
+import pandas as pd 
+
+df = pd.read_csv('breast-cancer.data')
+df.replace('?',-99999, inplace=True)
+df.drop(['id'],1,inplace=True)
+
+x = np.array(df.drop(['class'],1))
+y = np.array(df['class'])
+
+xtrain, xtest, ytrain, ytest = train_test_split(x,y,test_size=0.5)
+
+clf = neighbors.KNeighborsClassifier()
+clf.fit(xtrain,ytrain)
+
+acc = clf.score(xtest,ytest)
+
+print(acc)
+
+example= np.array([[4,2,1,1,1,2,3,2,1],[4,2,1,1,1,2,3,2,1]])
+example= example.reshape(len(example),-1)
+pred = clf.predict(example)
+print(pred)
---
+++ classification/euclidean.py
@@ -0,0 +1,10 @@
+from math import sqrt
+
+p = [1,3]
+q = [2,5]
+
+dis = sqrt( (p[0]-q[0])**2 + (p[1]-q[1])**2 )
+
+print(dis)
+
+x
