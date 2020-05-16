80ae83bd6409abd90c14ae113360cf47048d10ad
Sandi Fajar Rodiyansyah <galuh29@gmail.com>
2020-05-16 17:30:22 +0700

    Update kmean_from_strach.py

--- clustering/kmean_from_strach.py
+++ clustering/kmean_from_strach.py
@@ -18,7 +18,33 @@ colors = ["r.", "g.", "c.", "b.", "k." ]
 
 class K_Means:
     def __init__(self, k=2, tol=0.001, max_iter=300):
-        
+        self.k = k
+        self.tol = tol
+        self.max_iter = max_iter
+
+    def fit(self, data):
+        self.centroids = {}
+        for i in range(self.k):
+            self.centroids[i] = data[i]
+
+        for i in range(self.max_iter):
+            self.classifications = {}
+            for i in range(self.k):
+                self.classifications[i]=[]
+
+            for featureset in data:
+                distances = [np.linalg.norm(featureset - self.centroids[centroid]) for centroid in self.centroids]
+                classification = distances.index(min(distances))
+                self.classifications[classification].append(featureset)
+
+            prev_centroids = dict(self.centroids)
+
+            for classification in self.classifications:
+                self.centroids[classification] = np.average(self.classifications[classification], axis=0)
+
+    def predict(self, data):
+        pass
+
 
 
 
